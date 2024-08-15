1c81509fb5bb1f3873f56b4c24363fc113372055
bilalsaleem <bilalsaleem394@gmail.com>
2024-08-14 23:00:06 -0700

    Newchanges

---
+++ HomeTasks/hometask_javascript_files/obj.js
@@ -0,0 +1,8 @@
+const products = {
+    name: 'Delicious Apples',
+    price: 7.93
+}
+const productName = document.getElementById('product-name');
+productName.innerHTML = products.name;
+const productPrice = document.getElementById('product-price');
+productPrice.innerHTML = products.price;
