4dde101a3dbd1a020e20887b214525f921858226
bilalsaleem <bilalsaleem394@gmail.com>
2024-08-23 00:58:44 -0700

    Newchanges

--- HomeTasks/hometask_javascript_files/task4_events.js
+++ HomeTasks/hometask_javascript_files/task4_events.js
@@ -1,20 +1,24 @@
 //Generate a Random Number and shown on a any Html Tag
 const Result1 = document.getElementById('numResult1');
-Result1.innerHTML = generatRandomNumber ();
+Result1.innerHTML = generatRandomNumber();
 const btnClick = document.getElementById('btn-one');
 
-function generatRandomNumber (){
+Result1.innerHTML= " ";
+function generatRandomNumber() {
     return Math.floor(Math.random() * 90);
 }
 
-btnClick.addEventListener('click', () =>{
-    const finalresult = generatRandomNumber ();
-    Result1.innerHTML = `Random Number : ${finalresult}`
+btnClick.addEventListener('click', () => {
+    const finalresult = generatRandomNumber();
+    Result1.innerHTML = `Random Number: ${finalresult}`
 })
 
 //Show the input value with random colors.
 
 
+const inputResult = document.getElementById('numberGenerator')
+const Result2 = document.getElementById('numResult2')
+// This function generate random colors
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -22,20 +26,23 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+
 }
 
-
+// This function generate colorizedcharacters
 function colorizeCharacters(text) {
     let coloredText = '';
     for (let i = 0; i < text.length; i++) {
         const color = getRandomColor();
-        coloredText += '<span style="color:' + color + '">' + text[i] + '</span>';
+        coloredText += `<span style="color:${color}">${text[i]}</span>`;
     }
     return coloredText;
 }
+// console.log(colorizeCharacters())
 
-document.getElementById('numberGenerator').addEventListener('input', function() {
+
+inputResult.addEventListener('input', function () {
     const inputText = this.value;
     const result = colorizeCharacters(inputText);
-    document.getElementById('numResult2').innerHTML = result;
+    Result2.innerHTML = result;
 });
